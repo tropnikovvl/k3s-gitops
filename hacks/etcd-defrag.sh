dbad5897d1750307206ce1151aa44797993dec25
Christian Birkenbeul <github@teqqy.de>
2022-08-05 19:57:54 +0200

    add etcd defrag scripts

---
+++ hacks/etcd-defrag.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+export ETCDCTL_ENDPOINTS="https://127.0.0.1:2379"
+export ETCDCTL_CACERT="/var/lib/rancher/k3s/server/tls/etcd/server-ca.crt"
+export ETCDCTL_CERT="/var/lib/rancher/k3s/server/tls/etcd/server-client.crt"
+export ETCDCTL_KEY="/var/lib/rancher/k3s/server/tls/etcd/server-client.key"
+export ETCDCTL_API=3
+etcdctl defrag --cluster
